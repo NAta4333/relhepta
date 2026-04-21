71e948e2c031e10f683e5bc929a357a7622012d4
NEwera <saravinaleksandr8@gmail.com>
2026-04-21 13:10:34 +0300

    Update supportedGames.lua

--- supportedGames.lua
+++ supportedGames.lua
@@ -1,17 +1,12 @@
--- supportedGames.lua
 return {
-    -- Fallen Survival
     {
-        gameName = "[UP] Just a baseplate.",
-        placeIDs = {123974602339071},  
-        executors = {                
-            "Potassium"
+        gameName = "Just-a-baseplate",
+        placeIDs = {123974602339071},
+        executors = {"Potassium"},
+        customMessage = {
+            ["Volt"] = "Unstable",
         },
-        customMessage = {             
-            ["Fluxus"] = "Unstable",  
-            ["Electron"] = "Experimental" 
-        },
-        gitPath = "https://raw.githubusercontent.com/NAta4333/relhepta/refs/heads/main/fallen.lua",   
-        status = "Undetected"          
+        gitPath = "fallensurvival",
+        status = "Undetected"
     }
 }
